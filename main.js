3a1b726c7fb6264402efca0df206ace721407236
Carlos Sosa Rivero <81424281+sosarivero@users.noreply.github.com>
2024-03-24 00:21:06 +0000

    Use borders for better visibility. Format documents.

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-const level = "#####|#@$.#|#####";
+const level = "#######|#     #|#     #|# @$. #|#     #|#     #|#######";
 
 const grid = level.split("|").map((row) => row.split(""));
 console.log(grid);
 
 function draw(grid) {
-    const htmlGrid = document.createElement("div");
-    htmlGrid.id = "sokobanScreen";
-    for (row of grid) {
-        const htmlRow = document.createElement("div");
-        htmlRow.classList.add("row");
-        for (cell of row) {
-            const htmlCell = document.createElement("div");
-            htmlCell.textContent = cell;
-            htmlCell.classList.add("cell");
-            htmlRow.appendChild(htmlCell);
-        }
-        htmlGrid.appendChild(htmlRow);
+  const htmlGrid = document.createElement("div");
+  htmlGrid.id = "sokobanScreen";
+  for (row of grid) {
+    const htmlRow = document.createElement("div");
+    htmlRow.classList.add("row");
+    for (cell of row) {
+      const htmlCell = document.createElement("div");
+      htmlCell.textContent = cell;
+      htmlCell.classList.add("cell");
+      htmlRow.appendChild(htmlCell);
     }
-    document.body.appendChild(htmlGrid);
+    htmlGrid.appendChild(htmlRow);
+  }
+  document.body.appendChild(htmlGrid);
 }
 
 draw(grid);
