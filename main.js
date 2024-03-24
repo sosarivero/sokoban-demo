35fed50c4b5ffa3fdb365419f9f8967b630621c2
Carlos Sosa Rivero <81424281+sosarivero@users.noreply.github.com>
2024-03-24 10:03:47 +0000

    Add WASD movement through empty spaces

--- main.js
+++ main.js
@@ -1,23 +1,37 @@
 const level = "#######|#     #|#     #|# @$. #|#     #|#     #|#######";
 
 const grid = level.split("|").map((row) => row.split(""));
-console.log(grid);
 
 function draw(grid) {
   const htmlGrid = document.createElement("div");
   htmlGrid.id = "sokobanScreen";
   for (row of grid) {
+    // Creates rows
     const htmlRow = document.createElement("div");
     htmlRow.classList.add("row");
     for (cell of row) {
       const htmlCell = document.createElement("div");
+      // Attaches the element symbol to the cell div.
       htmlCell.textContent = cell;
       htmlCell.classList.add("cell");
+      // Appends the cell to the row
       htmlRow.appendChild(htmlCell);
     }
+    // Appends the row to the grid
     htmlGrid.appendChild(htmlRow);
   }
+  // Appends the grid to the HTML body
   document.body.appendChild(htmlGrid);
+}
+
+function clear() {
+  const htmlGrid = document.querySelector("#sokobanScreen");
+  document.body.removeChild(htmlGrid);
+}
+
+function redraw(newGrid) {
+  clear();
+  draw(newGrid);
 }
 
 function findPlayer(grid) {
@@ -25,7 +39,6 @@ function findPlayer(grid) {
     const row = grid[y];
     if (row.includes("@")) {
       const x = row.indexOf("@");
-      console.log(y);
       return [y, x];
     }
   }
@@ -33,6 +46,44 @@ function findPlayer(grid) {
   return null;
 }
 
+function move(direction, grid) {
+  const playerPos = findPlayer(grid);
+  let playerY = playerPos[0];
+  let playerX = playerPos[1];
+
+  const key = direction.key;
+  switch (key) {
+    case "w":
+      if (grid[playerY - 1][playerX] === " ") {
+        grid[playerY][playerX] = " ";
+        grid[playerY - 1][playerX] = "@";
+      }
+      break;
+    case "s":
+      if (grid[playerY + 1][playerX] === " ") {
+        grid[playerY][playerX] = " ";
+        grid[playerY + 1][playerX] = "@";
+      }
+      break;
+    case "a":
+      if (grid[playerY][playerX - 1] === " ") {
+        grid[playerY][playerX] = " ";
+        grid[playerY][playerX - 1] = "@";
+      }
+      break;
+    case "d":
+      if (grid[playerY][playerX + 1] === " ") {
+        grid[playerY][playerX] = " ";
+        grid[playerY][playerX + 1] = "@";
+      }
+      break;
+  }
+  redraw(grid);
+}
+
+window.addEventListener("keydown", (e) => {
+  move(e, grid);
+});
+
 draw(grid);
 const x = findPlayer(grid);
-console.log(x);
